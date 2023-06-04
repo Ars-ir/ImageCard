907e59a2a76c2d272afe4262e1cab51f9e1fb44e
Arfan Siregar <arfan.siregar@kpvendor.id>
2023-06-04 23:30:42 +0700

    - Define contentScale for Crop

--- app/src/main/java/com/ascoder/imagecard/MainActivity.kt
+++ app/src/main/java/com/ascoder/imagecard/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -38,7 +39,8 @@ class MainActivity : ComponentActivity() {
            Box(modifier = Modifier.height(200.dp)) {
                Image(
                    painter = painter,
-                   contentDescription = contentDescription
+                   contentDescription = contentDescription,
+                   contentScale = ContentScale.Crop
                )
            }
        }
