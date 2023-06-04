1473605515d9802681bbc93729d5a21335735868
Arfan Siregar <arfan.siregar@kpvendor.id>
2023-06-04 23:24:38 +0700

    - Adding shape with RoundedCornerShape for 15.dp

--- app/src/main/java/com/ascoder/imagecard/MainActivity.kt
+++ app/src/main/java/com/ascoder/imagecard/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +29,7 @@ class MainActivity : ComponentActivity() {
     ) {
        Card(
            modifier = modifier.fillMaxWidth(),
+           shape = RoundedCornerShape(15.dp)
        ) {
 
        }
