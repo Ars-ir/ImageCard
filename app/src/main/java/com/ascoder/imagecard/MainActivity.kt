b8d55b29e7f21c7659eebed469cfdb067064521e
Arfan Siregar <arfan.siregar@kpvendor.id>
2023-06-04 23:18:16 +0700

    - Adding component container function - Adding preview component

--- app/src/main/java/com/ascoder/imagecard/MainActivity.kt
+++ app/src/main/java/com/ascoder/imagecard/MainActivity.kt
@@ -3,12 +3,25 @@ package com.ascoder.imagecard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            ComponentContainer()
         }
+    }
+
+    @Composable
+    fun ComponentContainer() {
+        
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun ContainerPreview() {
+        ComponentContainer()
     }
 }
